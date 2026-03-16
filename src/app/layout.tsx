edba4f9506d455a3388cc09ebc3c9594f38b482b
phamngockhanh <ngockhanhpham8a@gmail.com>
2026-03-16 13:47:38 +0700

    01: setup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,18 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+import { Inter, IBM_Plex_Mono } from "next/font/google";
 
-const geistSans = Geist({
+import { ThemeProvider } from "@/components/theme-provider";
+
+import "./globals.css";
+const inter = Inter({
   variable: "--font-geist-sans",
   subsets: ["latin"],
 });
 
-const geistMono = Geist_Mono({
+const plexMono = IBM_Plex_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
+  weight : ["400","500","600","700"],
 });
 
 export const metadata: Metadata = {
@@ -23,11 +26,16 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${inter.variable} ${plexMono.variable} antialiased`}
       >
-        {children}
+        <ThemeProvider
+        attribute="class"
+        defaultTheme="light"
+        enableSystem
+        disableTransitionOnChange
+      >{children}</ThemeProvider>
       </body>
     </html>
   );
